680350c6b908a7e4247cb63bcfffe7c7a3a0264b
aronramos33 <aronramos33@gmail.com>
2022-12-30 13:12:02 +0100

    Registro creado
    He actualizado la ventana de registro del cliente.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
  22/12/2022 -> Creación definitiva del proyecto de prácticas.
 
-26/12/2022 -> Diseño de la página principal del proyex+cto.
+26/12/2022 -> Diseño de la página principal del proyecto.
+
+30/12/2022 -> Creación y edición de la ventana de registro del cliente.
--- zIPC-TrabajoFinal/src/javafxmlapplication/VentanaPrincipalController.java
+++ zIPC-TrabajoFinal/src/javafxmlapplication/VentanaPrincipalController.java
@@ -5,12 +5,19 @@
  */
 package javafxmlapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -33,5 +40,18 @@ public class VentanaPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void registrarse(ActionEvent event) throws IOException {
+        FXMLLoader miCargador = new FXMLLoader(getClass().getResource("VentanaRegistroCliente.fxml"));
+        Parent root = miCargador.load();
+        
+        Scene scene = new Scene(root, 800, 450);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Registrarse");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
     
 }
